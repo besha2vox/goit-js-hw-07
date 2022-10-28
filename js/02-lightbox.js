82a77061f42d59aebd923347c7cee814febf20de
Serhii Revva <107750742+besha2vox@users.noreply.github.com>
2022-10-28 21:38:25 +0300

    complete task#2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+const galleryCardSet = galleryItems.map(createGalleryCard).join('');
+
+galleryRef.insertAdjacentHTML('afterbegin', galleryCardSet);
+
+galleryRef.addEventListener('click', onOpenModal);
+
+function createGalleryCard({ preview, original, description }) {
+    return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+      />
+    </a>
+  </div>`;
+}
+
+function onOpenModal(e) {
+    e.preventDefault();
+    const isImage = e.target.classList.contains('gallery__image');
+
+    if (!isImage) {
+        return;
+    }
+    return new SimpleLightbox('.gallery a', {
+        captionsData: 'alt',
+        captionDelay: 250,
+        closeText: 'Close',
+    });
+}
